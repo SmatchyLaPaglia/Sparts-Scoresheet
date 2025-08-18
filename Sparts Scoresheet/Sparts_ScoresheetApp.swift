768927ddffd41379bba8e30856b6d3c57db1b036
Jesse Wonder Clark <jwc@jessewonderclark.com>
2025-08-18 15:22:28 -0400

    Initial Commit

---
+++ Sparts Scoresheet/Sparts_ScoresheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sparts_ScoresheetApp.swift
+//  Sparts Scoresheet
+//
+//  Created by Jesse Wonder Clark on 8/18/25.
+//
+
+import SwiftUI
+
+@main
+struct Sparts_ScoresheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Sparts ScoresheetTests/Sparts_ScoresheetTests.swift
@@ -0,0 +1,17 @@
+//
+//  Sparts_ScoresheetTests.swift
+//  Sparts ScoresheetTests
+//
+//  Created by Jesse Wonder Clark on 8/18/25.
+//
+
+import Testing
+@testable import Sparts_Scoresheet
+
+struct Sparts_ScoresheetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
